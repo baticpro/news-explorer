f6bad4d31b7cba32d63cc6c3f2e9b0989e86622d
batraz.dzhioev <baticpro@gmail.com>
2020-01-09 18:21:34 +0300

    update lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,67 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all"
+  ],
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-native',
+    'jest'
+  ],
+  overrides: [
+    {
+      files: [
+        '*.{spec,test}.{js,ts,tsx}',
+        '**/__{mocks,tests}__/**/*.{js,ts,tsx}',
+      ],
+      env: {
+        jest: true,
+        'jest/globals': true,
+      },
+      rules: {
+        'react-native/no-inline-styles': 0,
+      },
+    }
+  ],
+  globals: {
+    __DEV__: true,
+    __dirname: false,
+    __fbBatchedBridgeConfig: false,
+    alert: false,
+    cancelAnimationFrame: false,
+    cancelIdleCallback: false,
+    clearImmediate: true,
+    clearInterval: false,
+    clearTimeout: false,
+    console: false,
+    document: false,
+    ErrorUtils: false,
+    escape: false,
+    Event: false,
+    EventTarget: false,
+    exports: false,
+    fetch: false,
+    FormData: false,
+    global: false,
+    Intl: false,
+    Map: true,
+    module: false,
+    navigator: false,
+    process: false,
+    Promise: true,
+    requestAnimationFrame: true,
+    requestIdleCallback: true,
+    require: false,
+    Set: true,
+    setImmediate: true,
+    setInterval: false,
+    setTimeout: false,
+    WebSocket: true,
+    window: false,
+    XMLHttpRequest: false,
+  },
 };
