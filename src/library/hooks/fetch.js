79c25bd214c3cff2e23d463f9b7e3bde64cecf18
batraz.dzhioev <baticpro@gmail.com>
2020-01-10 16:11:25 +0300

    add useFetch hook

---
+++ src/library/hooks/fetch.js
@@ -0,0 +1,53 @@
+import {useState, useEffect} from 'react';
+import {API_URL, API_KEY} from 'src/config/config';
+
+export const useFetch = ({path, params}) => {
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const onSuccess = responseData => {
+			setData(responseData);
+			setLoading(false);
+		};
+
+		const onError = error => {
+			setError(error);
+			setLoading(false);
+		};
+
+		setLoading(true);
+
+		loadData({
+			path,
+			params,
+			onSuccess,
+			onError,
+		});
+	}, []);
+
+	return {data, loading, error};
+};
+
+const loadData = async ({path, params, onSuccess, onError}) => {
+	try {
+		const d = await fetch(buildUrl(path, params));
+		const data = await d.json();
+
+		onSuccess(data);
+	} catch (e) {
+		console.log(e);
+		onError(e);
+	}
+};
+
+const buildUrl = (path, params) => {
+	let url = API_URL + '/' + path + '?apiKey=' + API_KEY;
+
+	for (let paramName in params) {
+		url += '&' + params[paramName];
+	}
+
+	return url;
+};
